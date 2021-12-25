c03846135b7c25ec32170f5234aa6130333b4049
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-12-25 22:02:50 +0530

    setup the project

---
+++ src/main/java/com/mypractice/dgs/graphql/SbDsgGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.dgs.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDsgGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDsgGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypractice/dgs/graphql/SbDsgGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.dgs.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDsgGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
